10f63c6cf58f0cb287ca654ffaa61d5374902558
Ekeso <ekesolonge@gmail.com>
2023-01-11 10:39:55 +0100

    implement blade layout

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Faker\Provider\Lorem;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+
+        \App\Models\Listing::factory(6)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
